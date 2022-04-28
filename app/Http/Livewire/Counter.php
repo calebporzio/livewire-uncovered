3122ac8ec528f4d3b494e8a905ec55f0b12a7e85
Caleb Porzio <calebporzio@gmail.com>
2022-04-28 11:21:14 -0400

    wip

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Livewire;
 
-class Counter extends \Livewire\Component
+class Counter
 {
     public $count = 0;
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,28 @@
 <html>
     <link href="/app.css" rel="stylesheet">
 
-    <livewire:counter />
-
-    @livewireScripts
+    {!! livewire(App\Http\Livewire\Counter::class) !!}
 </html>
+
+<?php
+
+function livewire($class) {
+    $component = new $class;
+
+    return Blade::render(
+        $component->render(),
+        getProperties($component)
+    );
+}
+
+function getProperties($component) {
+    $properties = [];
+
+    $reflectedProperties = (new ReflectionClass($component))->getProperties(ReflectionProperty::IS_PUBLIC);
+
+    foreach ($reflectedProperties as $property) {
+        $properties[$property->getName()] = $property->getValue($component);
+    }
+
+    return $properties;
+}
