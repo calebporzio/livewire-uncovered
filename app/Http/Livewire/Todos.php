fc63eda12752f57f57d8312a402d8d4e4cc5545f
Caleb Porzio <calebporzio@gmail.com>
2022-05-02 11:58:09 -0400

    wip

---
+++ app/Http/Livewire/Todos.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+class Todos
+{
+    public $draft = '';
+
+    public $todos = ['One todo', 'Two todo'];
+
+    public function addTodo()
+    {
+        $this->todos[] = $this->draft;
+
+        $this->draft = '';
+    }
+
+    public function render()
+    {
+        return <<<'HTML'
+            <div class="todos">
+                <input type="text" wire:model="draft">
+
+                <button wire:click="addTodo">Add Todo</button>
+
+                <ul>
+                    @foreach ($todos as $todo)
+                    <li>{{ $todo }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        HTML;
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <script defer src="https://unpkg.com/@alpinejs/morph@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    @livewire(App\Http\Livewire\Counter::class)
+    @livewire(App\Http\Livewire\Todos::class)
 
     <script src="/livewire.js"></script>
 </html>
